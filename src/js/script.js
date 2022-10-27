97a59c2c5d7b2494c7e0904a43c8d6b26fee8f5c
Denis Kholin <den07.06@yandex.ru>
2022-10-27 04:17:54 +0300

    fixed modals

--- src/js/script.js
+++ src/js/script.js
@@ -67,7 +67,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	//********************************************************************/
 
 	class PizzaCard {
-		constructor(pizzaName, img, alt, price, ingridient, dataIngridient, smallImg, bigImg, smallPrice, bigPrice, size, smallSize, bigSize, weight, smallWeight, bigWeight, id) {
+		constructor(pizzaName, img, alt, price, ingridient, dataIngridient, smallImg, bigImg, smallPrice, bigPrice, size, smallSize, bigSize, weight, smallWeight, bigWeight, id, serialNumber) {
 			this.pizzaName = pizzaName;
 			this.img = img;
 			this.alt = alt;
@@ -85,15 +85,16 @@ window.addEventListener('DOMContentLoaded', () => {
 			this.smallWeight = smallWeight;
 			this.bigWeight = bigWeight;
 			this.id = id;
+			this.serialNumber = serialNumber;
 			this.addPizzaCard();
-			this.createModal();
+			// this.createModal();
 		}
 
 		addPizzaCard() {
 			const pizzaItem = document.createElement('div');
 			pizzaItem.classList.add('pizza__item');
 			pizzaItem.setAttribute('data-ingridient', this.dataIngridient);
-			pizzaItem.setAttribute('id', this.id);
+			pizzaItem.setAttribute('data-id', this.id);
 
 			pizzaItem.innerHTML = `
 					<img src="${this.img}" alt="${this.alt}" class="pizza__item_img">
@@ -101,7 +102,7 @@ window.addEventListener('DOMContentLoaded', () => {
 					<p class="pizza__item_ingridient">${this.ingridient}</p>
 					<div class="pizza__item_control">
 						<p class="pizza__item_price">от ${this.price} руб.</p>
-						<button class="pizza__item_btn">Выбрать</button>
+						<button data-serialNumber="${this.serialNumber}" class="pizza__item_btn">Выбрать</button>
 					</div>
 				`;
 
@@ -109,67 +110,51 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 
 		createModal() {
-			const modalContent = document.createElement('div');
+			const
+				modal = document.querySelector('.modal'),
+				modalContent = document.createElement('div'),
+				modalDialog = document.querySelector('.modal__dialog');
 
 			modalContent.classList.add('modal__content');
-			modalContent.setAttribute('id', `modal${this.id}`);
+			modalContent.setAttribute('data-id', `${this.id}`);
+
+			modal.classList.remove('hide');
+			modal.classList.add('flexShow', 'fade');
+
+			modalDialog.innerHTML = '';
+			modalDialog.append(modalContent);
 
 			modalContent.innerHTML = `
-				<div class="modal__close">&times;</div>
-				<div class="modal__pizza">
-					<img src="${this.img}" alt="${this.alt}" class="modal__pizza_img">
-				</div>
-				<div class="modal__info">
-					<div class="modal__info_content">
-						<p class="modal__name">${this.pizzaName}</p>
-						<p class="modal__description">${this.size}, традиционное тесто, ${this.weight}</p>
-						<p class="modal__ingridient">${this.ingridient}</p>
-						<div class="modal__switch">
-							<input type="radio" name="size${this.id}" id="small${this.id}" class="modal__radio modal__radio_small" value="small">
-							<label for="small${this.id}" class="modal__label">Маленькая</label>
-
-							<input type="radio" name="size${this.id}" id="medium${this.id}" class="modal__radio modal__radio_medium" value="medium" checked>
-							<label for="medium${this.id}" class="modal__label">Средняя</label>
-
-							<input type="radio" name="size${this.id}" id="big${this.id}" class="modal__radio modal__radio_big" value="big">
-							<label for="big${this.id}" class="modal__label">Большая</label>
-						</div>
+					<div class="modal__close">&times;</div>
+					<div class="modal__pizza">
+						<img src="${this.img}" alt="${this.alt}" class="modal__pizza_img">
 					</div>
-					<button class="modal__button">Добавить в корзину за ${this.price} руб.</button>
-				</div>
-			`;
-
-			document.querySelector('.modal__dialog').append(modalContent);
-
-			const modal = document.querySelector('.modal');
-
-			document.querySelectorAll('.pizza__item_btn').forEach(el => {
-				el.addEventListener('click', (ev) => {
-
-					modal.classList.remove('hide');
-					modal.classList.add('flexShow');
-
-
-					document.querySelectorAll('.modal__content').forEach(el => {
-						el.classList.remove('show');
-						el.classList.add('hide');
-					})
-
-					document.getElementById(`modal${ev.target.parentNode.parentNode.getAttribute('id')}`).classList.remove('hide');
-					document.getElementById(`modal${ev.target.parentNode.parentNode.getAttribute('id')}`).classList.add('show');
-					document.body.style.overflow = 'hidden';
-				})
-			})
-
+					<div class="modal__info">
+						<div class="modal__info_content">
+							<p class="modal__name">${this.pizzaName}</p>
+							<p class="modal__description">${this.size}, традиционное тесто, ${this.weight}</p>
+							<p class="modal__ingridient">${this.ingridient}</p>
+							<div class="modal__switch">
+								<input type="radio" name="size${this.id}" id="small${this.id}" class="modal__radio modal__radio_small" value="small">
+								<label for="small${this.id}" class="modal__label">Маленькая</label>
+	
+								<input type="radio" name="size${this.id}" id="medium${this.id}" class="modal__radio modal__radio_medium" value="medium" checked>
+								<label for="medium${this.id}" class="modal__label">Средняя</label>
+	
+								<input type="radio" name="size${this.id}" id="big${this.id}" class="modal__radio modal__radio_big" value="big">
+								<label for="big${this.id}" class="modal__label">Большая</label>
+							</div>
+						</div>
+						<button class="modal__button">Добавить в корзину за ${this.price} руб.</button>
+					</div>
+				`;
 
 
 			function hideModal(modalSelector) {
-				modalSelector.classList.remove('flexShow');
+				modalSelector.classList.remove('flexShow', 'fade');
 				modalSelector.classList.add('hide');
 				document.body.style.overflow = '';
 			}
-
-
 
 			document.addEventListener('keydown', (ev) => {
 				if (ev.code == "Escape" && getComputedStyle(modal).display == 'flex') {
@@ -183,22 +168,17 @@ window.addEventListener('DOMContentLoaded', () => {
 				}
 
 				if (ev.target.classList.contains('modal__radio_small')) {
-					// modalContent.querySelector('.modal__pizza_img').setAttribute('src', this.smallImg);
-					modalContent.querySelector('.modal__pizza_img').remove();
-					document.querySelector('.modal__pizza').innerHTML = `
-					<img src="${this.smallImg}" alt="${this.alt}" class="modal__pizza_img">
-					`
+					modalContent.querySelector('.modal__pizza_img').setAttribute('src', this.smallImg);
+				}
 
+				if (ev.target.classList.contains('modal__radio_medium')) {
+					modalContent.querySelector('.modal__pizza_img').setAttribute('src', this.img);
+				}
 
-					// ev.target.parentElement.parentElement.parentElement.previousElementSibling.children[0].setAttribute('src', this.smallImg);
-
-
+				if (ev.target.classList.contains('modal__radio_big')) {
+					modalContent.querySelector('.modal__pizza_img').setAttribute('src', this.bigImg);
 				}
 			})
-
-			// document.getElementById(`small${this.id}`).addEventListener('click', () => {
-
-			// })
 
 		}
 	}
@@ -218,14 +198,29 @@ window.addEventListener('DOMContentLoaded', () => {
 	fetch('db.json')
 		.then((response) => response.json())
 		.then((data) => {
+
+			const pizzaArray = [];
+			let serialNumber = 0;
+
 			data.pizza.forEach(elem => {
 
 				const { pizzaName, img, alt, price, ingridient, dataIngridient, smallImg, bigImg, smallPrice, bigPrice, size, smallSize, bigSize, weight, smallWeight, bigWeight, id } = elem;
 
 				if (checkNotEmpty(elem)) {
-					new PizzaCard(pizzaName, img, alt, price, ingridient, dataIngridient, smallImg, bigImg, smallPrice, bigPrice, size, smallSize, bigSize, weight, smallWeight, bigWeight, id)
+					pizzaArray.push(new PizzaCard(pizzaName, img, alt, price, ingridient, dataIngridient, smallImg, bigImg, smallPrice, bigPrice, size, smallSize, bigSize, weight, smallWeight, bigWeight, id, serialNumber))
 				}
 
+				++serialNumber;
+			})
+			return pizzaArray;
+		})
+		.then((data) => {
+			document.querySelectorAll('.pizza__item_btn').forEach(el => {
+				el.addEventListener('click', ev => {
+					let cardNumber = ev.target.getAttribute('data-serialNumber');
+					console.log(cardNumber);
+					data[cardNumber].createModal();
+				})
 			})
 		});
 })
