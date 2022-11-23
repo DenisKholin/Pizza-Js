72ff08492e25b120e637ac945dc799737fc6eb9e
Denis Kholin <den07.06@yandex.ru>
2022-11-23 22:24:44 +0300

    Remove method 'createCartItem' from Class

--- src/js/script.js
+++ src/js/script.js
@@ -211,98 +211,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			}
 		}
 
-		createCartItem(src, size, weight, price, count, dataId, alt, pizzaName) {
-			const cartItem = document.createElement('div');
-			cartItem.classList.add('cart__item');
-			cartItem.setAttribute('data-id', dataId);
-
-			cartItem.innerHTML = `
-				<div class="cart__item_info">
-					<img src="${src}" alt="${alt}" class="cart__item_img">
-					<div class="cart__item_text">
-						<p class="cart__item_title">${pizzaName}</p>
-						<p class="cart__item_description">
-							<span class="modal__description_size">${size}</span>
-							, традиционное тесто,
-							<span class="modal__description_weight">${weight}</span>
-							г
-						</p>
-					</div>
-					<p class="cart__item_close">&times;</p>
-				</div>
-				<div class="cart__item_line"></div>
-				<div class="cart__item_control">
-					<p class="cart__item_price">${price} руб.</p>
-					<div class="cart__item_panel">
-						<button class="cart__item_minus cart__item_button" type="button">-</button>
-						<p class="cart__item_count">${count}</p>
-						<button class="cart__item_plus cart__item_button" type="button">+</button>
-					</div>
-				</div>
-				<div class="cart__item_delete">
-					<p class="cart__delete_text">Хотите удалить из корзины?</p>
-					<div class="cart__delete">
-						<button type="button" class="cart__delete_yes cart__delete_button">Да</button>
-						<button type="button" class="cart__delete_no cart__delete_button">Нет</button>
-					</div>
-				</div>
-			`
-
-			document.querySelector('.cart__product').append(cartItem);
-
-			const
-				cartItemCount = cartItem.querySelector('.cart__item_count'),
-				cartItemPrice = cartItem.querySelector('.cart__item_price'),
-				priceOfOne = +cartItem.querySelector('.cart__item_price').innerHTML.slice(0, -5);
-
-			cartItem.querySelector('.cart__item_plus').addEventListener('click', () => {
-				cartItemCount.innerHTML++;
-				cartCount++;
-				this.refreshCartitem(cartItemPrice, priceOfOne, cartItemCount);
-			})
-
-			cartItem.querySelector('.cart__item_minus').addEventListener('click', () => {
-				if (+cartItemCount.innerHTML <= 1) {
-					this.deleteCartItem(cartItem, dataId);
-				} else {
-					cartItemCount.innerHTML--;
-					cartCount--;
-					this.refreshCartitem(cartItemPrice, priceOfOne, cartItemCount);
-				}
-			})
-
-			cartItem.querySelector('.cart__item_close').addEventListener('click', () => {
-				this.deleteCartItem(cartItem, dataId);
-			})
-		}
-
-		refreshCartitem(price, priceOfOne, count) {
-			price.innerHTML = ((priceOfOne * count.innerHTML).toFixed(2)) + ' руб.';
-			refreshCartCount()
-			calculateTotalPrice();
-		}
-
-		deleteCartItem(cartItem, dataId) {
-			this.toggleCartItemDelete(cartItem, 1, 2);
-
-			cartItem.querySelector('.cart__delete_yes').addEventListener('click', () => {
-				cartItem.remove();
-				calculateTotalPrice();
-				cartCount--;
-				refreshCartCount()
-				cartArray.splice(cartArray.indexOf(dataId), 1);
-
-			})
-			cartItem.querySelector('.cart__delete_no').addEventListener('click', () => {
-				this.toggleCartItemDelete(cartItem, 0, -2);
-			})
-		}
-
-		toggleCartItemDelete(cartItem, opacity, zIndex) {
-			cartItem.querySelector('.cart__item_delete').style.opacity = opacity;
-			cartItem.querySelector('.cart__item_delete').style.zIndex = zIndex;
-		}
-
 	}
 
 	const checkNotEmpty = (obj) => {
@@ -313,6 +221,99 @@ window.addEventListener('DOMContentLoaded', () => {
 			return true;
 		}
 	}
+
+	function createCartItem(src, size, weight, price, count, dataId, alt, pizzaName) {
+		const cartItem = document.createElement('div');
+		cartItem.classList.add('cart__item');
+		cartItem.setAttribute('data-id', dataId);
+
+		cartItem.innerHTML = `
+			<div class="cart__item_info">
+				<img src="${src}" alt="${alt}" class="cart__item_img">
+				<div class="cart__item_text">
+					<p class="cart__item_title">${pizzaName}</p>
+					<p class="cart__item_description">
+						<span class="modal__description_size">${size}</span>
+						, традиционное тесто,
+						<span class="modal__description_weight">${weight}</span>
+						г
+					</p>
+				</div>
+				<p class="cart__item_close">&times;</p>
+			</div>
+			<div class="cart__item_line"></div>
+			<div class="cart__item_control">
+				<p class="cart__item_price">${price} руб.</p>
+				<div class="cart__item_panel">
+					<button class="cart__item_minus cart__item_button" type="button">-</button>
+					<p class="cart__item_count">${count}</p>
+					<button class="cart__item_plus cart__item_button" type="button">+</button>
+				</div>
+			</div>
+			<div class="cart__item_delete">
+				<p class="cart__delete_text">Хотите удалить из корзины?</p>
+				<div class="cart__delete">
+					<button type="button" class="cart__delete_yes cart__delete_button">Да</button>
+					<button type="button" class="cart__delete_no cart__delete_button">Нет</button>
+				</div>
+			</div>
+		`
+
+		document.querySelector('.cart__product').append(cartItem);
+
+		const
+			cartItemCount = cartItem.querySelector('.cart__item_count'),
+			cartItemPrice = cartItem.querySelector('.cart__item_price'),
+			priceOfOne = +cartItem.querySelector('.cart__item_price').innerHTML.slice(0, -5);
+
+		cartItem.querySelector('.cart__item_plus').addEventListener('click', () => {
+			cartItemCount.innerHTML++;
+			cartCount++;
+			refreshCartitem(cartItemPrice, priceOfOne, cartItemCount);
+		})
+
+		cartItem.querySelector('.cart__item_minus').addEventListener('click', () => {
+			if (+cartItemCount.innerHTML <= 1) {
+				deleteCartItem(cartItem, dataId);
+			} else {
+				cartItemCount.innerHTML--;
+				cartCount--;
+				refreshCartitem(cartItemPrice, priceOfOne, cartItemCount);
+			}
+		})
+
+		cartItem.querySelector('.cart__item_close').addEventListener('click', () => {
+			deleteCartItem(cartItem, dataId);
+		})
+	}
+
+	function refreshCartitem(price, priceOfOne, count) {
+		price.innerHTML = ((priceOfOne * count.innerHTML).toFixed(2)) + ' руб.';
+		refreshCartCount()
+		calculateTotalPrice();
+	}
+
+	function deleteCartItem(cartItem, dataId) {
+		toggleCartItemDelete(cartItem, 1, 2);
+
+		cartItem.querySelector('.cart__delete_yes').addEventListener('click', () => {
+			cartItem.remove();
+			calculateTotalPrice();
+			cartCount--;
+			refreshCartCount()
+			cartArray.splice(cartArray.indexOf(dataId), 1);
+
+		})
+		cartItem.querySelector('.cart__delete_no').addEventListener('click', () => {
+			toggleCartItemDelete(cartItem, 0, -2);
+		})
+	}
+
+	function toggleCartItemDelete(cartItem, opacity, zIndex) {
+		cartItem.querySelector('.cart__item_delete').style.opacity = opacity;
+		cartItem.querySelector('.cart__item_delete').style.zIndex = zIndex;
+	}
+
 
 	function callCreateModal(clickTrigger, data) {
 		document.querySelectorAll(clickTrigger).forEach(el => {
@@ -342,7 +343,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 						cartArray.push(dataId);
 
-						data[serialNumber].createCartItem(currentSrc, currentSize, currentWeight, currentPrice, 1, dataId, currentAlt, currentTitle);
+						createCartItem(currentSrc, currentSize, currentWeight, currentPrice, 1, dataId, currentAlt, currentTitle);
 
 						updateLocalStorage(currentTitle, currentSize, currentWeight, 1, currentPrice, currentSrc, dataId, currentAlt)
 
@@ -421,7 +422,7 @@ window.addEventListener('DOMContentLoaded', () => {
 					let json = JSON.parse(localStorage.getItem(`pizza-${JSON.parse(localStorage.getItem('arrOfId'))[i]}`));
 
 					cartArray.push(json.dataId)
-					data[i].createCartItem(json.src, json.size, json.weight, json.price, json.count, json.dataId, json.alt, json.name);
+					createCartItem(json.src, json.size, json.weight, json.price, json.count, json.dataId, json.alt, json.name);
 				}
 
 				calculateTotalPrice();
